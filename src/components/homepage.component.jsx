0f8e1d622626afe7e363c439c7283d3260a9d0ec
Shammael <shammamnd2015@gmail.com>
2021-04-21 18:28:24 -0400

    first commit with homepage without a real style

---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,40 @@
+import './homepage.styles.scss';
+
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <p className="title">HATS</p>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <p className="title">JACKETS</p>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <p className="title">SNEAKERS</p>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <p className="title">WOMENS</p>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <p className="title">MENS</p>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
